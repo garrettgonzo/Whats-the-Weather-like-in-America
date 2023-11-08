0150f938e72740b75a31fc184b29d229086f6a94
Garrett Gonzales <gggonzo4@yahoo.com>
2023-11-08 12:16:24 -0800

    added wheater auto updating code

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -5,5 +5,22 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&ap
 fetch(queryURL).then(response => {
     response.json().then(response => {
         console.log(response);
+        const htmlString = `
+            <main>
+                <h1>temp:${response.main.temp} </h1>
+                <h1>humidity:${response.main.humidity} </h1>
+                <p>Learn how to convert a string to HTML using JavaScript.</p>
+            </main>
+        `;
+        
+        const convertStringToHTML = htmlString => {
+            const parser = new DOMParser();
+            const html = parser.parseFromString(htmlString, 'text/html');
+        
+            return html.body;
+        }
+        const body = convertStringToHTML(htmlString);
+        
+        document.body.appendChild(body)
     })
 })
