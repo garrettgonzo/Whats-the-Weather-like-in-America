e4f8270940b26f60ee2c2a0817aeecfd756c2c7d
Garrett Gonzales <gggonzo4@yahoo.com>
2023-11-09 00:05:39 -0800

    added button

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,3 +1,9 @@
+var cityButton = document.querySelector(".cityButton")
+cityButton.addEventListener("click", function(){
+    console.log("click");
+});
+
+
 console.log("dc7271cf24099ef606a3cfa6b3d2e65f");
 var APIKey = "dc7271cf24099ef606a3cfa6b3d2e65f";
 const city = "London"
@@ -7,8 +13,11 @@ fetch(queryURL).then(response => {
         console.log(response);
         const htmlString = `
             <main>
-                <h1>temp:${response.main.temp} </h1>
-                <h1>humidity:${response.main.humidity} </h1>
+            <h1>Location: ${response.name} </h1> 
+            <h1>Weather: ${response.weather[0].main} </h1>
+            <h1>Temp: ${response.main.temp} °F</h1>
+            <h1>Wind: ${response.wind.speed} MPH</h1>
+                <h1>Humidity: ${response.main.humidity} %</h1>
                 <p>Learn how to convert a string to HTML using JavaScript.</p>
             </main>
         `;
@@ -24,3 +33,11 @@ fetch(queryURL).then(response => {
         document.body.appendChild(body)
     })
 })
+
+// const myarry = [50,60,70,80]
+//     const myObject = {
+//         foo: 42,
+//         bar: 17,
+//         monke: 16,
+//       };
+// console.log(myarry[-1]);
