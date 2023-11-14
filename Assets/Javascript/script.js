427594b1246c01c6698755b986ddf7368e9256aa
Garrett Gonzales <gggonzo4@yahoo.com>
2023-11-13 20:51:14 -0800

    edited the css design to look good, and added the to the README file

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,3 +1,18 @@
+const resultsElement = document.getElementById('results');
+// Set the HTML content of the 'results' element
+// const responseHTML = `
+//             <main>
+//             <h6>Location:  </h6> 
+//             <h6>Weather: <img src="https://openweathermap.org/img/w/10d.png" alt="Girl in a jacket">   </h6>
+//             <h6>Temp:  °F</h6>
+//             <h6>Wind:  MPH</h6>
+//             <h6>Humidity:  %</h6>
+//             <h6>Country:  </h6>
+//         <p>Learn how to convert a string to HTML using JavaScript.</p>
+//         </main>
+//         `;
+// resultsElement.innerHTML = responseHTML;
+
 // console.log('hi')
 // const searchButton = document.querySelector("#searchBtn");
 // console.log(searchButton)
@@ -13,82 +28,81 @@ const handleSearch = () => {
     // console.log(inputSearchValue);
     
     var APIKey = "dc7271cf24099ef606a3cfa6b3d2e65f";
-    const city = inputSearchValue
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    const presentCity = inputSearchValue
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + presentCity + "&units=imperial&appid=" + APIKey;
     fetch(queryURL).then(response => {
         response.json().then(response => {
             // console.log(response);
             const htmlString = `
-            <main>
-            <h6>Location: ${response.name} </h6> 
-            <h6>Weather: ${response.weather[0].icon} </h6>
-            <h6>Temp: ${response.main.temp} °F</h6>
-            <h6>Wind: ${response.wind.speed} MPH</h6>
-            <h6>Humidity: ${response.main.humidity} %</h6>
-            <h6>Country: ${response.sys.country} </h6>
-        <p>Learn how to convert a string to HTML using JavaScript.</p>
+            <main class="presentDay">
+            <h6 class="dates">Location: ${response.name} </h6>
+            <h6>Weather: <img src="https://openweathermap.org/img/w/${response.weather[0].icon}.png" alt="Girl in a jacket">   </h6>
+            <h6 class="futureText">Temp: ${response.main.temp} °F</h6>
+            <h6 class="futureText">Wind: ${response.wind.speed} MPH</h6>
+            <h6 class="futureText">Humidity: ${response.main.humidity} %</h6>
+            <h6 class="futureText">Country: ${response.sys.country} </h6>
         </main>
         `;
+        resultsElement.innerHTML = htmlString;
         
-        const convertStringToHTML = htmlString => {
-            const parser = new DOMParser();
-            const html = parser.parseFromString(htmlString, 'text/html');
+        // const convertStringToHTML = htmlString => {
+        //     const parser = new DOMParser();
+        //     const html = parser.parseFromString(htmlString, 'text/html');
             
-            return html.body;
-        }
-        const body = convertStringToHTML(htmlString);
+        //     return html.body;
+        // }
+        // const body = convertStringToHTML(htmlString);
         
-        document.body.appendChild(body)
+        // document.body.appendChild(body)
     })
 })
 
-}
-searchBtn.addEventListener("click", handleSearch);
-
 var APIKey = "dc7271cf24099ef606a3cfa6b3d2e65f";
-const city = "brea"
+const city = inputSearchValue
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?&q=" + city + "&units=imperial&appid=" + APIKey
 fetch(queryURL).then(response => {
     response.json().then(response => {
         console.log(response);
         const htmlString = `
-        <main>
-        <h1>Location: ${response.city.name} </h1> 
-        <h1>Date: ${response.list[4].dt_txt} </h1> 
-        <h1>Weather: ${response.list[4].weather[0].icon} </h1> 
-        <h1>Temp: ${response.list[4].main.temp} °F</h1> 
-        <h1>Wind: ${response.list[4].wind.speed} MPH</h1> 
-        <h1>Humidity: ${response.list[4].main.humidity} %</h1> 
+        <main class="futureDay">
+        <p class="fiveDay">Your 5 Day Forcast!☀️</p>
+        <h1 class="dates">Date: ${response.list[4].dt_txt} </h1> 
+        <h1 class="futureText">Weather: <img src="https://openweathermap.org/img/w/${response.list[4].weather[0].icon}.png"></h6>
+        <h1 class="futureText">Temp: ${response.list[4].main.temp} °F</h1> 
+        <h1 class="futureText">Wind: ${response.list[4].wind.speed} MPH</h1> 
+        <h1 class="futureText">Humidity: ${response.list[4].main.humidity} %</h1> 
 
-        <h2>Location: ${response.city.name} </h2> 
-        <h2>Date: ${response.list[12].dt_txt} </h2> 
-        <h2>Weather: ${response.list[12].weather[0].icon} </h2> 
-        <h2>Temp: ${response.list[12].main.temp} °F</h2> 
-        <h2>Wind: ${response.list[12].wind.speed} MPH</h2> 
-        <h2>Humidity: ${response.list[12].main.humidity} %</h2>
+        <h2 class="dates">Date: ${response.list[12].dt_txt} </h2> 
+        <h2 class="futureText">Weather: <img src="https://openweathermap.org/img/w/${response.list[12].weather[0].icon}.png"></h6>
+        <h2 class="futureText">Temp: ${response.list[12].main.temp} °F</h2> 
+        <h2 class="futureText">Wind: ${response.list[12].wind.speed} MPH</h2> 
+        <h2 class="futureText">Humidity: ${response.list[12].main.humidity} %</h2>
+         
+        <h3 class="dates">Date: ${response.list[20].dt_txt} </h3>
+        <h3 class="futureText">Weather: <img src="https://openweathermap.org/img/w/${response.list[20].weather[0].icon}.png"></h6>  
+        <h3 class="futureText">Temp: ${response.list[20].main.temp} °F</h3> 
+        <h3 class="futureText">Wind: ${response.list[20].wind.speed} MPH</h3> 
+        <h3 class="futureText">Humidity: ${response.list[20].main.humidity} %</h3>
         
-        <h3>Location: ${response.city.name} </h3> 
-        <h3>Date: ${response.list[20].dt_txt} </h3> 
-        <h3>Weather: ${response.list[20].weather[0].icon} </h3> 
-        <h3>Temp: ${response.list[20].main.temp} °F</h3> 
-        <h3>Wind: ${response.list[20].wind.speed} MPH</h3> 
-        <h3>Humidity: ${response.list[20].main.humidity} %</h3>
-        
-        <h4>Location: ${response.city.name} </h4> 
-        <h4>Date: ${response.list[28].dt_txt} </h4> 
-        <h4>Weather: ${response.list[28].weather[0].icon} </h4> 
-        <h4>Temp: ${response.list[28].main.temp} °F</h4> 
-        <h4>Wind: ${response.list[28].wind.speed} MPH</h4> 
-        <h4>Humidity: ${response.list[28].main.humidity} %</h4>
-        
-        <h5>Location: ${response.city.name} </h5> 
-        <h5>Date: ${response.list[36].dt_txt} </h5> 
-        <h5>Weather: ${response.list[36].weather[0].icon} </h5> 
-        <h5>Temp: ${response.list[36].main.temp} °F</h5> 
-        <h5>Wind: ${response.list[36].wind.speed} MPH</h5> 
-        <h5>Humidity: ${response.list[36].main.humidity} %</h5> 
+        <h4 class="dates">Date: ${response.list[28].dt_txt} </h4>
+        <h4 class="futureText">Weather: <img src="https://openweathermap.org/img/w/${response.list[28].weather[0].icon}.png"></h6> 
+        <h4 class="futureText">Temp: ${response.list[28].main.temp} °F</h4> 
+        <h4 class="futureText">Wind: ${response.list[28].wind.speed} MPH</h4> 
+        <h4 class="futureText">Humidity: ${response.list[28].main.humidity} %</h4>
+         
+        <h5 class="dates">Date: ${response.list[36].dt_txt} </h5> 
+        <h5 class="futureText">Weather: <img src="https://openweathermap.org/img/w/${response.list[36].weather[0].icon}.png"></h6>  
+        <h5 class="futureText">Temp: ${response.list[36].main.temp} °F</h5> 
+        <h5 class="futureText">Wind: ${response.list[36].wind.speed} MPH</h5> 
+        <h5 class="futureText">Humidity: ${response.list[36].main.humidity} %</h5> 
     </main>
     `;
+    // function addimage() {
+    //     var img = new Image();
+    //     img.src = `https://openweathermap.org/img/w/${response.list[36].weather[0].icon}.png`
+    //     img_home.appendChild(img);
+    // }
+    // addimage()
     
     const convertStringToHTML = htmlString => {
         const parser = new DOMParser();
@@ -101,3 +115,6 @@ fetch(queryURL).then(response => {
     document.body.appendChild(body)
     })
 })
+}
+searchBtn.addEventListener("click", handleSearch);
+
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Whats-the-Weather-like-in-America
+
+## User Story
+
+AS A student
+
+I WANT to build a fully functional weather website
+
+SO THAT my website can present the weather in any given for any given city in America
+
+## Acceptance Criteria
+
+GIVEN a that the website is fully functionable
+
+WHEN I successfully assemble a working website with a working API that allows the user to access any city
+
+WHEN I create a working search bar
+
+WHEN I create a good-looking design
+
+WHEN I have the API display the place, weather icon, temp, wind speed, humidity and country
+
+When I have a good-looking NAV bar
+
+When I have programmed the API to display the next 5 day weather forcasts for the selected city
+
+When I have learned how to use and implement an API key into my website
+
+WHEN I the website is pushed out and works with ease
+
+## Deployed URL
+
+## Website Screenshot
+
+![Whats the Weather like in America Screenshot](<Screenshot (1149).png>)
